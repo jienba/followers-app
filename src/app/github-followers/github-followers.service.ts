4d12dd8f8fe29b175e8f65358f54dfecc552b4f7
Jienba <adamjienba@outlook.fr>
2022-02-21 16:14:05 +0000

    Let's Deploy it on GithubPages 😃

--- src/app/github-followers/github-followers.service.ts
+++ src/app/github-followers/github-followers.service.ts
@@ -1,14 +1,15 @@
-import { Http } from '@angular/http';
-import { Injectable } from '@angular/core';
+import {Http} from '@angular/http';
+import {Injectable} from '@angular/core';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class GithubFollowersService {
-  private readonly _url = 'https://api.github.com/users/mosh-hamedani/followers';
+  private readonly _url = 'https://api.github.com/users/mamadou-wone/followers';
 
-  constructor(private http: Http) {  }
+  constructor(private http: Http) {
+  }
 
-  getFollowers() { 
+  getFollowers() {
     return this.http.get(this._url)
       .map(response => response.json());
   }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {environment} from '../../environments/environment';
 
 @Component({
   selector: 'navbar',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  // navbarBackgroundColor = environment.navBarbackgroundColor;
   constructor() { }
 
   ngOnInit() {
